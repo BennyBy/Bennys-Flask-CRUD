807eada599f911dd003e2a13e925e06262e34900
BennyBy <benny@fusionsys.co.il>
2025-04-17 20:26:30 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -9,3 +9,14 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>' 
+
+<tr>
+  <td>{{ user.username }}</td>
+  <td>{{ user.email }}</td>
+  <td>
+    <form action="{{ url_for('main.delete_user', user_id=user.id) }}" method="POST" style="display:inline;">
+      <button type="submit" onclick="return confirm('Are you sure you want to delete this user?');">Delete</button>
+    </form>
+  </td>
+</tr>
+
